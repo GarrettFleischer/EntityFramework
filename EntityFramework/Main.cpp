3c58f4de45d7b2d5629b8e9f010f5a55a90dd085
GarrettFleischer <garrett.fleischer@oit.edu>
2016-04-21 21:05:32 -0700

    Added check for memory leaks

--- EntityFramework/Main.cpp
+++ EntityFramework/Main.cpp
@@ -1,3 +1,8 @@
+#ifdef _DEBUG
+#define _CRTDBG_MAP_ALLOC
+#define new new( _CLIENT_BLOCK, __FILE__, __LINE__)
+#endif // _DEBUG
+
 #include "EntityManager.h"
 #include "RenderSystem.h"
 #include "GraphicsComponent.h"
@@ -8,11 +13,13 @@ using namespace EntitySystem;
 
 int main()
 {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
 	EntityManager manager;
 	RenderSystem sys_render(&manager);
 
 	// player
-	manager.CreateEntity({ new PlayerComponent(), new GraphicsComponent() });
+	manager.CreateEntity({ new PlayerComponent(), new PositionComponent(7, 8), new GraphicsComponent() });
 
 	// bushes
 	manager.CreateEntity({ new GraphicsComponent('*', 2), new PositionComponent(3, 3) });
@@ -21,11 +28,13 @@ int main()
 	manager.CreateEntity({ new GraphicsComponent('*', 2), new PositionComponent(12, 10) });
 
 	// Update the manager and systems
-	while (true)
-	{
+	//while (true)
+	//{
 		manager.Update();
 		sys_render.Update();
-	}
+	//}
+
+
 
 	return 0;
 }
